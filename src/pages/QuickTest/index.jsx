a4fd7fb7edc24d040a8a705726dac73198ba4e7e
JeganMurugan <jegan.murugan@concertidc.com>
2024-08-15 11:54:02 +0530

    chnages in Test

--- src/pages/QuickTest/index.jsx
+++ src/pages/QuickTest/index.jsx
@@ -1,26 +1,17 @@
 import { useState } from "react";
 
 export const QuickTest = () => {
-  const [imageUrl, setImageUrl] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleInputChange = (setter) => (e) => setter(e.target.value);
   const handleFileChange = (e) => setSelectedFile(e.target.files[0]);
 
-  const handleSubmit = () => {
-    const source = imageUrl || selectedFile?.name;
-    if (source) {
-      console.log(`Processing image from ${imageUrl ? "URL" : "local file"}: ${source}`);
-    }
-  };
-
   return (
-    <div className="h-screen grid grid-cols-12 gap-4 p-6 ">
-      <div className="col-span-8 p-5 flex items-center justify-center">
-        <div className="border-2 border-gray-300 w-full h-full flex items-center justify-center">
-          {imageUrl || selectedFile ? (
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-12 gap-4 p-6">
+      <div className="md:col-span-8 p-5 ">
+        <div className="border-2 border-gray-300 w-10/12 h-64 md:h-3/6 flex items-center justify-center">
+          {selectedFile ? (
             <img
-              src={imageUrl || URL.createObjectURL(selectedFile)}
+              src={URL.createObjectURL(selectedFile)}
               alt="Test"
               className="object-cover w-full h-full"
             />
@@ -30,25 +21,10 @@ export const QuickTest = () => {
         </div>
       </div>
 
-      <div className="col-span-4 p-5">
-        <label className="block mb-2 font-semibold">Image URL</label>
-        <input
-          type="text"
-          placeholder="Enter Image URL"
-          value={imageUrl}
-          onChange={handleInputChange(setImageUrl)}
-          className="p-2 border border-gray-300 rounded-lg w-full mb-4"
-        />
-        <button
-          onClick={handleSubmit}
-          className="bg-blue-500 text-white font-medium py-2 px-4 rounded-lg hover:bg-blue-600 w-full mb-4"
-        >
-          Submit
-        </button>
-        <p className="mb-4 text-center text-gray-500">or</p>
+      <div className="md:col-span-4 p-5 flex flex-col">
         <label
           htmlFor="file-upload"
-          className="bg-blue-500 text-white font-medium py-2 px-[104px] rounded-lg hover:bg-blue-600 w-full mb-4 cursor-pointer"
+          className="bg-blue-500 text-white font-medium py-2 text-center px-4 md:px-[8%] rounded-lg hover:bg-blue-600 w-full mb-4 cursor-pointer"
         >
           Browse local files
           <input
