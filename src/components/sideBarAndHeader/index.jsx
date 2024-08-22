0ee17d921b0deb7b66bd7a51a0219908bc98666d
JeganMurugan <jegan.murugan@concertidc.com>
2024-08-22 13:59:20 +0530

    added some changes"

--- src/components/sideBarAndHeader/index.jsx
+++ src/components/sideBarAndHeader/index.jsx
@@ -129,7 +129,7 @@ const SideBarHeader = ({
                   }}
                   className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-white cursor-pointer"
                 >
-                  AI poc
+                  Skin Disease Prediction
                 </span>
               </p>
             </div>
@@ -141,7 +141,7 @@ const SideBarHeader = ({
                       onClick={() => nav("/")}
                       className="text-gray-900 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700"
                     >
-                      Dashboard
+                      Home
                     </button>
                     <button
                       onClick={handleTrainModule}
